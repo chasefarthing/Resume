b1c9fa9df13ecb6fcf18310206bf3793f535ac1f
Chase <chasefarthing@me.com>
2014-07-23 16:40:18 -0700

    stable ish

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,27 @@
 var gulp = require('gulp');
-
-var less      = require('gulp-less');
-var minifycss = require('gulp-minify-css');
-var bless = require('gulp-bless');
-
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-
-var notify     = require('gulp-notify');
-var clean      = require('gulp-clean');
 var livereload = require('gulp-livereload');
-var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
 
 
 // LESS
-gulp.task('less', function() {
-    return gulp.src('less/style.less')
-        .pipe(less().on('error', notify.onError(function (error) {
-            return 'Error compiling LESS: ' + error.message;
-        })))
-        // .pipe(minifycss())
-        // .pipe(bless({
-        //     imports: true
-        // }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(livereload())
-        .pipe(notify({
-            message: 'Successfully compiled LESS'
-        }));
+gulp.task('sass', function() {
+    return gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(''));
 });
 
-// Linting
-gulp.task('lint', function() {
-    return gulp.src('js/script.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(livereload());
-});
 
-// JavaScript
-gulp.task('js', function() {
-    return gulp.src([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/bootstrap/js/transition.js',
-            'bower_components/bootstrap/js/collapse.js',
-            'bower_components/bootstrap/js/carousel.js',
-            'bower_components/bootstrap/js/dropdown.js',
-            'bower_components/bootstrap/js/modal.js',
-            'js/script.js'
-        ])
-        .pipe(concat('script.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-        .pipe(livereload())
-        .pipe(notify({
-            message: 'Successfully compiled JS'
-        }));
-});
 
-// Clean
-gulp.task('clean', function() {
-    return gulp.src(['dist/css', 'dist/js'], {read: false})
-        .pipe(clean());
-});
+
 
 // Default task
-gulp.task('default', ['clean'], function() {
-    gulp.run('less', 'lint', 'js');
+gulp.task('default', function() {
+    gulp.run('sass');
 });
 
-// Watch
 gulp.task('watch', function() {
     livereload.listen();
-    // Watch .less files
-    gulp.watch('less/**/*.less', ['less']);
-
-    // Watch .js files
-    gulp.watch('js/**/*.js', ['lint', 'js']);
+    gulp.watch('sass/*.scss', ['sass']).on('change', livereload.changed);
+    gulp.watch('*').on('change', livereload.changed);
 });
